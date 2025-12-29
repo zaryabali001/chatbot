107f08601876ab60ba268721bf96b2b9be47cdcf
zaryab <azaryab820@gmail.com>
2025-12-29 16:16:57 +0500

    component name change9

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import Sana from "@/components/Sana";
+
+export const dynamic = "force-dynamic";
 
 
 export default function Page() {
   return (
     <div className="bg-transparent h-screen  w-screen overflow-hidden">
       {/* Hospital Chatbot */}
-      <Sana />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Sana />
+      </Suspense>
     </div>
   )
 }
