9c42b13ea8a6e0c0075396c20278da14fb037ea8
zaryab <azaryab820@gmail.com>
2026-01-20 15:35:50 +0500

    content changings

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-none bg-transparent shadow-none ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-none bg-transparent shadow-none border-none ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
